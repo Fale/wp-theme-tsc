10212b69634274e1a0538b1f9dedf3462630c7fd
Fabio Alessandro Locati <flocati@grimp.eu>
2010-11-24 23:05:05 +0100

    Better credits

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 	<div id="footer" role="contentinfo">
 Parrocchia S. Ambrogio ad Fontes, Via Cardinal Schuster 1, 20090 Villaggio Ambrosiano di Segrate,<br />
 tel. 02.2131196, e-mail : info@santambrogiosegrate.org<br />
-Sito creato da Grimp di Fabio Alessandro Locati
+Sito creato da <a href="http://grimp.eu">Grimp di Fabio Alessandro Locati</a>
 	</div><!-- #footer -->
 </div><!-- #wrapper -->
 
