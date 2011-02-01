fd455e479be33ff81945e13f2f88a5f85834c658
Fabio Alessandro Locati <flocati@grimp.eu>
2011-02-01 14:09:37 +0100

    Pointless to  be checked

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,9 +1,6 @@
 <div id="footer-widget-area" role="complementary">
 	<ul class="xoxo">
-		<?php
-		    if (is_active_sidebar( 'first-footer-widget-area' ))
-		        dynamic_sidebar( 'first-footer-widget-area' );
-		?>
+		<?php dynamic_sidebar( 'footer-widget-area' ); ?>
 		Sito creato da <a href="http://grimp.eu">Grimp di Fabio Alessandro Locati</a>
 	</ul>
 </div>
