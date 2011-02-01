c3ede48505283731f9b4ab0182b6a625406f09d8
Fabio Alessandro Locati <flocati@grimp.eu>
2011-02-01 12:34:40 +0100

    Forgot the bottom widget bar

---
+++ sidebar-footer.php
@@ -0,0 +1,9 @@
+<div id="footer-widget-area" role="complementary">
+	<ul class="xoxo">
+		<?php
+		    if (is_active_sidebar( 'first-footer-widget-area' ))
+		        dynamic_sidebar( 'first-footer-widget-area' );
+		?>
+		Sito creato da <a href="http://grimp.eu">Grimp di Fabio Alessandro Locati</a>
+	</ul>
+</div>
