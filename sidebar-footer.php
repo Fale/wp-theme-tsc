d1a95bd9255174b92dab4636e754ae35aa39d36c
Fabio Alessandro Locati <flocati@grimp.eu>
2011-02-01 16:00:37 +0100

    Better this footer

--- sidebar-footer.php
+++ sidebar-footer.php
@@ -1,6 +1,6 @@
 <div id="footer-widget-area" role="complementary">
 	<ul class="xoxo">
 		<?php dynamic_sidebar( 'footer-widget-area' ); ?>
-		Sito creato da <a href="http://grimp.eu">Grimp di Fabio Alessandro Locati</a>
+		Sito creato da <a href="http://grimp.eu">Grimp</a>
 	</ul>
 </div>
